4a7b9b0a5d4b8d2812934aa749c9afd93aae53cc
Federico <f.ullua@gmail.com>
2023-06-08 20:53:27 -0300

    fix itemlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./styles/components.css";
 import NavBar from "./components/NavBar";
 import { Box } from "@chakra-ui/react";
-import data from "./data.json";
 import Catalogo from "./components/Catalogo"
 
 function App() {
@@ -24,27 +23,6 @@ function App() {
   );
 }
 
-//async
-const getData = () => {
-  return new Promise((resolve, reject) => {
-    if (data.length === 0) {
-      reject(new Error("No hay datos"));
-    }
-    setTimeout(() => {
-      resolve(data);
-    }, 2000);
-  });
-};
 
-async function fetchingData() {
-  try {
-    const datosFetched = await getData();
-    console.log(datosFetched);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-fetchingData();
 
 export default App;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,4 +1,29 @@
+import data from "../data.json";
+import itemList from "../components/itemList";
+
 const ItemListContainer = () => {
+  //async
+const getData = () => {
+  return new Promise((resolve, reject) => {
+    if (data.length === 0) {
+      reject(new Error("No hay datos"));
+    }
+    setTimeout(() => {
+      resolve(data);
+    }, 2000);
+  });
+};
+
+async function fetchingData() {
+  try {
+    const datosFetched = await getData();
+    console.log(datosFetched);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+fetchingData();
   return (
     <div>ItemListContainer</div>
   )
