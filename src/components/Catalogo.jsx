40b1e59d93c57d09643aef9c40a558d139555e27
Federico <f.ullua@gmail.com>
2023-06-09 00:07:43 -0300

    faltan las rutas

--- src/components/Catalogo.jsx
+++ src/components/Catalogo.jsx
@@ -1,15 +1,19 @@
 import data from "../data.json";
 
+const autoimg = ""
+
 const Catalogo = () => {
   return (
     <>
-    <div><h1 className="titulos" >Listado de Productos Disponibles</h1></div>
+      <div>
+        <h1 className="titulos">Listado de Productos Disponibles</h1>
+      </div>
       {data.map((prod) => {
         return (
           <div key={prod.id}>
             <h3>{prod.producto}</h3>
+            <img className="carimg" src="src/assets/img/auto-generico.jpg"/>
             <h5>{prod.descripcion}</h5>
-            <h5>Stock: {prod.stock}</h5>
           </div>
         );
       })}
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -5,11 +5,12 @@ const ItemDetail = () => {
 
     return (
         <>
-        <div><h1 className="titulos" >Listado de Productos Disponibles</h1></div>
+        <div><h1 className="titulos" >Detalle del Vehiculo</h1></div>
           {data.map((prod) => {
             return (
               <div key={prod.id}>
                 <h3>{prod.producto}</h3>
+                <img className="carimg" src="src/assets/img/auto-generico.jpg"/>
                 <h5>Descrición: {prod.descripcionLarga}</h5>
                 <h5>Stock: {prod.stock}</h5>
               </div>
